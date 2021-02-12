1e3551316368c2db0f44f72735b55068ce48ed9c
Xiaoqing Ding <bbq598@bu.edu>
2021-02-11 22:31:59 -0500

    init gateway, eureka, and cloud config
    init gateway, eureka, and cloud config

---
+++ test/src/main/java/com/bfs/test/TestController.java
@@ -0,0 +1,19 @@
+package com.bfs.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movie")
+public class TestController {
+    @Autowired
+    private CloudConfig cloudConfig;
+
+    @GetMapping("/message")
+    public ResponseEntity<String> getMessage() {
+        return ResponseEntity.ok("Message from " + cloudConfig.getUserRole()  );
+    }
+}
