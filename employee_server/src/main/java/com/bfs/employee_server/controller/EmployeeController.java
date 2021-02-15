2c02ab2baf40789d468047fb2f2637840d020d66
Lijunhao0916 <augustus0916@gmail.com>
2021-02-14 20:45:33 -0500

    add all

---
+++ employee_server/src/main/java/com/bfs/employee_server/controller/EmployeeController.java
@@ -0,0 +1,37 @@
+package com.bfs.employee_server.controller;
+
+import com.bfs.employee_server.client.EmployeeClient;
+import com.bfs.employee_server.config.CloudConfig;
+import com.bfs.employee_server.domain.Contact;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    @Autowired
+    private CloudConfig cloudConfig;
+
+    @Autowired
+    private EmployeeClient employeeClient;
+
+    @GetMapping("/message")
+    public ResponseEntity<String> getMessage() {
+        return ResponseEntity.ok("Message from " + cloudConfig.getUserRole()  );
+    }
+
+
+    @PostMapping("/getContactByUserId")
+    public Contact getContactByUserId(@RequestBody Map<String, String> map) {
+        return employeeClient.getContactByUserId(map);
+    }
+
+    @PostMapping("getFloatingDayByUserId")
+    public String getFloatingDayByUserId(@RequestBody Map<String, String> map) {
+        return employeeClient.getFloatingDayByUserId(map);
+    }
+}
