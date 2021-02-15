1986ad0dd1e9c680d1fbb40d717a310ccdf75478
Lijunhao0916 <augustus0916@gmail.com>
2021-02-14 21:22:10 -0500

    add yml

--- employee_core-server/src/main/java/com/bfs/employee_coreserver/EmployeeCoreServerApplication.java
+++ employee_core-server/src/main/java/com/bfs/employee_coreserver/EmployeeCoreServerApplication.java
@@ -2,6 +2,8 @@ package com.bfs.employee_coreserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 @SpringBootApplication
 public class EmployeeCoreServerApplication {
 
